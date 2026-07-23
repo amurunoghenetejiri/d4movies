7e5a930cc7654b324cf76650b2a847aab0fc3aa0
amurunoghenetejiri <amurunoghenetejiri@gmail.com>
2026-07-23 19:09:46 +0100

    Update Logo.tsx

--- src/components/brand/Logo.tsx
+++ src/components/brand/Logo.tsx
@@ -6,7 +6,7 @@ type Props = {
   glow?: boolean;
 };
 
-export function Logo({ size = 64, withText = false, className = "", glow = false }: Props) {
+export function Logo({ size = 78, withText = false, className = "", glow = false }: Props) {
   return (
     <div className={`flex items-center gap-2 ${className}`}>
       <img
